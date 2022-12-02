abc706a5786aa9bd27617766920ba2ce327f8524
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2022-12-02 14:55:46 -0500

    Create parse.php

---
+++ parsedown/parse.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'parsedown.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$docRoot = __DIR__;
+
+
+// return README.md if no file is specified
+if ($uri === '/' || strlen($uri) === 0) {
+    $uri = '/README';
+}
+
+if (file_exists($file = $docRoot . $uri .'.md')) {
+    $parsedown = new Parsedown();
+    echo $parsedown->text(file_get_contents($file));
+} else {
+    echo '404 - Not Found';
+}
